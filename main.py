97e341ef35dde179e030467fa6288359556ba384
osiruko419 <53327520+osiruko419@users.noreply.github.com>
2019-08-20 13:43:23 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--headless","-hl",type=int,default=0,help="headlessを設定")
     parser.add_argument("--pattern","-pa",type=int,default=1,help="ランダム0はrefreshなし、プロフ探索のwordを選ぶ、ファボ消し1でいいね済み、not24時間を削除")
-    parser.add_argument("--execute","-ex",type=int,default=0,help="0はランダム探索,1はプロフ探索,2はファボ消し,3はスクショ")
+    parser.add_argument("--execute","-ex",type=int,default=0,help="0はランダム探索,1はプロフ探索,2はファボ消し")
 
     args = parser.parse_args()
     
